6af97aee2d537bed0354992c8a472db96d0a98ae
ajijohn <aji.john@gmail.com>
2017-02-24 11:17:18 -0800

    Reformatted the code

--- R/DDFunctions.R
+++ R/DDFunctions.R
@@ -26,64 +26,86 @@ degree.days=function(Tmin,Tmax,LDT=NA,UDT=NA, method="single.sine"){
   theta1=asin(((LDT-(Tmax+Tmin)/2)/alpha)*pi/180)
   theta2=asin(((UDT-(Tmax+Tmin)/2)/alpha)*pi/180)
   
-#Single sine calculation
-if(method=="single.sine"){
-  
-# entirely above both thresholds
-if(Tmin>=LDT) {dd=(UDT-LDT)/2}
-
-#Intercepted by upper threshold
-if(Tmax>UDT && Tmin>LDT){
-  dd=1/pi*(((Tmax+Tmin)/2-LDT)*(theta2+ pi/2)+(UDT-LDT)*(pi/2-theta2)-alpha*cos(theta2))
-}
-    
-#Intercepted by both thresholds
-  if(Tmax>UDT && Tmin<LDT){
-    dd=1/pi*(((Tmax+Tmin)/2-LDT)*(theta2+ theta1)+alpha*(cos(theta1)-cos(theta2))+ (UDT-LDT)*(pi/2-theta2))
-  }
-  
-#Entirely between both thresholds
-  if(Tmax<UDT && Tmin>LDT) {dd=(Tmax-Tmin)/2-LDT}
-  
-# intercepted by LDT
-  ## CHECK 1/pi to 1/2pi?
-if(Tmin<LDT && Tmax>LDT){
-dd=1/pi*(((Tmax+Tmin)/2-LDT)*(pi/2-theta1)+alpha*cos(theta1))
-if(!is.na(dd))if(dd<0){dd=0}
-}
-
-# entirely below both thresholds
-if(Tmax<=LDT){ dd=0}
-} #end single sine method
-
-#---------------------------  
-#double sine calculation
-  if(method=="double.sine"){
+  #Single sine calculation
+  if (method == "single.sine") {
     
     # entirely above both thresholds
-    if(Tmin>=LDT) {dd=(UDT-LDT)/2}
+    if (Tmin >= LDT) {
+      dd = (UDT - LDT) / 2
+    }
     
     #Intercepted by upper threshold
-    if(Tmax>UDT && Tmin>LDT){
-      dd=1/(2*pi)*(((Tmax+Tmin)/2-LDT)*(theta2+ pi/2)+(UDT-LDT)*(pi/2-theta2)-alpha*cos(theta2))
+    else if (Tmax > UDT && Tmin > LDT) {
+      dd = 1 / pi * (((Tmax + Tmin) / 2 - LDT) * (theta2 + pi / 2) + (UDT - LDT) *
+                       (pi / 2 - theta2) - alpha * cos(theta2))
     }
     
     #Intercepted by both thresholds
-    if(Tmax>UDT && Tmin<LDT){
-      dd=1/(2*pi)*(((Tmax+Tmin)/2-LDT)*(theta2+ theta1)+alpha*(cos(theta1)-cos(theta2))+ (UDT-LDT)*(pi/2-theta2))
+    else if (Tmax > UDT && Tmin < LDT) {
+      dd = 1 / pi * (((Tmax + Tmin) / 2 - LDT) * (theta2 + theta1) + alpha * (cos(theta1) -
+                                                                                cos(theta2)) + (UDT - LDT) * (pi / 2 - theta2))
     }
     
     #Entirely between both thresholds
-    if(Tmax<UDT && Tmin>LDT) {dd=0.5*((Tmax-Tmin)/2-LDT)}
+    else if (Tmax < UDT && Tmin > LDT) {
+      dd = (Tmax - Tmin) / 2 - LDT
+    }
     
     # intercepted by LDT
-    if(Tmin<LDT && Tmax>LDT){
-      dd=1/(2*pi)*(((Tmax+Tmin)/2-LDT)*(pi/2-theta1)+alpha*cos(theta1))
-      if(!is.na(dd))if(dd<0){dd=0}
+    ## CHECK 1/pi to 1/2pi?
+    else if (Tmin < LDT && Tmax > LDT) {
+      dd = 1 / pi * (((Tmax + Tmin) / 2 - LDT) * (pi / 2 - theta1) + alpha * cos(theta1))
+      if (!is.na(dd))
+        if(dd < 0) {
+          dd = 0
+        }
     }
     
     # entirely below both thresholds
-    if(Tmax<=LDT){ dd=0}
+    else if (Tmax <= LDT) {
+      dd = 0
+    }
+  } #end single sine method
+
+#---------------------------  
+  #double sine calculation
+  if (method == "double.sine") {
+    # entirely above both thresholds
+    if (Tmin >= LDT) {
+      dd = (UDT - LDT) / 2
+    }
+    
+    #Intercepted by upper threshold
+    if (Tmax > UDT && Tmin > LDT) {
+      dd = 1 / (2 * pi) * (((Tmax + Tmin) / 2 - LDT) * (theta2 + pi / 2) + (UDT -
+                                                                              LDT) * (pi / 2 - theta2) - alpha * cos(theta2))
+    }
+    
+    #Intercepted by both thresholds
+    if (Tmax > UDT && Tmin < LDT) {
+      dd = 1 / (2 * pi) * (((Tmax + Tmin) / 2 - LDT) * (theta2 + theta1) + alpha *
+                             (cos(theta1) - cos(theta2)) + (UDT - LDT) * (pi / 2 - theta2))
+    }
+    
+    #Entirely between both thresholds
+    if (Tmax < UDT && Tmin > LDT) {
+      dd = 0.5 * ((Tmax - Tmin) / 2 - LDT)
+    }
+    
+    # intercepted by LDT
+    if (Tmin < LDT && Tmax > LDT) {
+      dd = 1 / (2 * pi) * (((Tmax + Tmin) / 2 - LDT) * (pi / 2 - theta1) + alpha *
+                             cos(theta1))
+      if (!is.na(dd))
+        if(dd < 0) {
+          dd = 0
+        }
+    }
+    
+    # entirely below both thresholds
+    if (Tmax <= LDT) {
+      dd = 0
+    }
   } #end double sine method
   
 return(dd)
