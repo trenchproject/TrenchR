7fdac3e21129654a7343c8ec087ff00e5e39ba0f
ajijohn <aji.john@gmail.com>
2017-03-02 11:41:39 -0800

    Added tests for single and double sine

---
+++ tests/testthat/test-dd-single-sine.R
@@ -0,0 +1,9 @@
+context("Degree Days::Single Sine")
+test_that("degree day function-single-sine work", {
+  singlesine = read.csv("./degree-days-single-sine.csv")
+  singlesine$DD.Trench<-degree.days(singlesine$Air.min,singlesine$Air.max,12,33,"single.sine")
+  corrsingsine=cor(singlesine$Degree.days,singlesine$DD.Trench,method = "pearson")
+  expect_gte(corrsingsine, .94)
+  rm(singlesine)
+  
+})
