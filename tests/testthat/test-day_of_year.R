816f69076f4b9df7c2bdd23239e954b41a481c00
ajijohn <aji.john@gmail.com>
2017-09-06 13:52:16 -0700

    Test scripts for utility functions

---
+++ tests/testthat/test-day_of_year.R
@@ -0,0 +1,12 @@
+context("Utility Functions::Day of the year")
+test_that("day_of_year function works as expected", {
+  # First day of the year
+  dayofyear = day_of_year("2017-01-01", format= "%Y-%m-%d")
+  expect_equal(dayofyear, 1)
+  # Summer Equinox
+  dayofyear =  day_of_year("2017-06-20", format= "%Y-%m-%d")
+  expect_equal(dayofyear, 171)
+  # Last day of the year
+  dayofyear = day_of_year("2017-12-31", format= "%Y-%m-%d")
+  expect_equal(dayofyear, 365)
+})
---
+++ tests/testthat/test-dec_angle.R
@@ -0,0 +1,11 @@
+context("Utility Functions::Declination Angle")
+test_that("dec_angle function works as expected", {
+  # Winter Solstice (Should be negative around -23.45 degrees , approx -0.41 radians)
+  winsol = day_of_year("2017-12-21", format= "%Y-%m-%d")
+  dec_angle_win_sols = dec_angle(winsol)
+  expect_lte(dec_angle_win_sols, -.40)
+  # Summer Equinox (Should be positive 23.45 degrees , approx 0.41 radians))
+  summerequi =  day_of_year("2017-06-20", format= "%Y-%m-%d")
+  expect_gte(summerequi, .40)
+  
+})
