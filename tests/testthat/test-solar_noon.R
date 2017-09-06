5a7ce6ef979e2cde5b9fa574fc321422439197e4
ajijohn <aji.john@gmail.com>
2017-09-06 14:50:12 -0700

    Test scripts for utility functions - Part 2

---
+++ tests/testthat/test-solar_noon.R
@@ -0,0 +1,10 @@
+context("Utility Functions::Solar noon")
+test_that("solar_noon function works as expected", {
+  # Vancouver - Solar noon in the month of June
+  june10 = day_of_year("2017-06-10", format= "%Y-%m-%d")
+  solar_noon_van = solar_noon(-123.117, june10)
+  
+  # Issue - Expect 12:12:02, but getting approx 11:45 am
+  
+
+})
