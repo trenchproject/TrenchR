f4d741759a9fff311be289095d4c9dad017275bb
ajijohn <aji.john@gmail.com>
2017-09-07 15:54:34 -0700

    Refactor pkg for biophys models

---
+++ R/biophysmodel_Grasshopper.R
@@ -0,0 +1,134 @@
+#' @details Predicts body temperatures (operative environmental temperature) of a grasshopper in degrees C.
+#' @description Predicts body temperature (operative environmental temperature) of a grasshopper in degrees C. Described in Buckleyet al. (2014, Phenotypic clines, energy balances, and ecological responses to climate change. Journal of Animal Ecology 83:41-50.) See also a related model by Anderson et al. (1979, Habitat selection in two species of short-horned grasshoppers. Oecologia 38:359–74.)
+#' 
+#' @param Ta is air temperature in C
+#' @param Tg  is surface temperature in C, Kingsolver (1983) assumes Tg-Ta=8.4
+#' @param u is wind speed in m/s
+#' @param rad  is total (direct + diffuse) solar radiation flux in W/m^2
+#' @param kt is the clearnex index (dimensionless), which is the ratio of the global solar radiation measured at the surface to the total solar radiation at the top of the atmosphere.
+#' @param psi is solar zenith angle in degrees
+#' @param L in grasshopper length in m
+#' @param Acondfact is the proportion of the grasshopper surface area that is in contact with the ground
+#' @param z is grasshopper's distance from the ground in m
+#' @param abs is absorptivity of grasshopper to solar radiation (proportion), See Anderson et al. (1979).
+#' @param r_g is substrate solar reflectivity (proportion), see Kingsolver (1983)
+#' 
+#' @keywords body temperature, biophysical model
+#' @export
+#' @examples
+#' \dontrun{
+#' estimate_grasshopper_Tb(Ta=25, Tg=25, u=0.4, rad=400, kt=0.7, psi=30, L=0.02, Acondfact=0.25, z=0.001, abs=0.7, r_g=0.3)
+#'}
+
+estimate_grasshopper_Tb=function(Ta, Tg, u, rad, kt, psi, L, Acondfact=0.25, z=0.001, abs=0.7, r_g=0.3){
+  
+TaK<- Ta+273.15 #Ambient temperature in K
+Tg<- Tg+273.15 #Ambient temperature in K
+
+#Biophysical parameters
+#IR emissivity
+omega<-5.66 * 10^-8 # stefan-boltzmann constant (W m^-2 K^-4)
+epsilon=1 #Gates 1962 in Kingsolver 1983  #emissivity of surface to longwave IR
+
+Kf=0.025  #Kf=0.024+0.00007*Ta[k] #thermal conductivity of fluid
+
+#kineamatic viscosity of air (m^2/s); http://users.wpi.edu/~ierardi/PDF/air_nu_plot.PDF
+v=15.68*10^-6  #m^2/s, kinematic viscocity of air,  at 300K #http://www.engineeringtoolbox.com/air-absolute-kinematic-viscosity-d_601.html
+
+#AREAS
+#Samietz (2005): The body of a grasshopper female was approximated by a rotational ellipsoid with half the body length as the semi-major axis q.
+#Area from Wolfram math world
+c<- L/2 #c- semi-major axis, a- semi-minor axis
+a<- (0.365+0.241*L*1000)/1000  #regression in Lactin and Johnson (1988)
+e=sqrt(1-a^2/c^2)
+A=2*pi*a^2+2*pi*a*c/e*asin(e)
+
+#------------------------------
+#SOLAR RADIATIVe HEAT FLUX   
+#Separate Total radiation into components
+#Use Erbs et al model from Wong and Chow (2001, Applied Energy 69:1991-224)
+
+#kd- diffuse fraction
+if(kt<=0.22) kd=1-0.09*kt
+if(kt>0.22 & kt<=0.8) kd= 0.9511 -0.1604*kt +4.388*kt^2 -16.638*kt^3 +12.336*kt^4
+if(kt>0.8) kd=0.165 #kd = 0.125 #Correction from 16.5 for CO from Olyphant 1984
+
+Httl=rad
+Hdir=Httl*(1-kd)
+Hdif=Httl*kd;     
+
+#------------------------------
+#Anderson 1979 - calculates Radiation as W without area dependence 
+psi_r=psi*pi/180 #psi in radians
+
+#Calculate Qabs as W
+Qdir=abs*Hdir/cos(psi_r) #direct radiation
+Qdif=abs*Hdif #diffuse radiation
+Qref= r_g *Httl #reflected radiation
+Qabs= Qdir + Qdif + Qref  #W/m2
+
+#------------------------------
+#convection
+
+#Reynolds number- ratio of interval viscous forces
+#L: Characeristic dimension (length)
+# u= windspeed #Lactin and Johnson add 1m/s to account for cooling by passive convection
+Re= u*L/v
+#Nusselt number- dimensionless conductance
+Nu=0.41* Re^0.5 #Anderson 1979 empirical
+h_c= Nu *Kf /L # heat transfer coefficient, Wm^{-2}C^{-1} #reported in Lactin and Johnson 1998
+
+hc_s<- h_c *(-0.007*z/L +1.71) # heat transfer coefficient in turbulent air 
+
+#conduction 
+Thick= 6*10^(-5) #cuticle thickness (m)
+hcut= 0.15 #W m^-1 K^-1
+Acond=A * Acondfact 
+#Qcond= hcut *Acond *(Tb- (Ta+273))/Thick
+
+#------------------------------
+#Energy balance based on Kingsolver (1983, Thermoregulation and flight in Colias butterflies: elevational patterns and mechanistic limitations. Ecology 64: 534–545).
+
+#Thermal radiative flux
+#Areas
+# silhouette area / total area
+sa<-0.19-0.00173*psi #empirical from Anderson 1979, psi in degrees
+Adir= A*sa
+Aref=Adir 
+
+#Calculate Qabs as W/m2
+Qdir=abs*Adir*Hdir/cos(psi_r)
+Qdif=abs*Aref*Hdif
+Qref= abs* r_g * Aref *Httl
+Qabs= Qdir + Qdif + Qref  #W/m2
+
+Tsky=0.0552*(Ta+273.15)^1.5; #Kelvin, black body sky temperature from Swinbank (1963), Kingsolver 1983 estimates using Brunt equation
+               
+#Qt= 0.5* A * epsilon * omega * (Tb^4 - Tsky^4) +0.5 * A * epsilon * omega * (Tb^4 - Tg^4) 
+#Convective heat flux
+#Qc= hc_s * A * (Tb- (Ta+273)) 
+#Qs= Qt+ Qc
+
+#WITH CONDUCTION
+#t solved in wolfram alpha #Solve[a t^4 +b t -d, t]
+a<- A * epsilon *omega
+b<-hc_s * A + hcut*Acond/Thick
+d<- hc_s*A*TaK +0.5*A*epsilon *omega*(Tsky^4+Tg^4)+ hcut *Acond*Tg/Thick +Qabs
+
+#WITHOUT CONDUCTION
+#a<- A * epsilon *omega
+#b<-hc_s * A
+#d<- hc_s*A*TaK +0.5*A*epsilon *omega*Tsky^4 +0.5*A*epsilon *omega*Tg^4 +Qabs
+
+#eb<-function(Tb) 0.5* A * epsilon * omega * (Tb^4 - Tsky^4) +0.5 * A * epsilon * omega * (Tb^4 - Tg^4) + hc_s * A * (Tb-TaK)+hcut *Acond *(Tb-Tg)/Thick -Qabs 
+#r <- uniroot(eb, c(-1,373), tol = 1e-5)
+#r$root-273
+
+#roots
+tb = 1/2*sqrt((2*b)/(a*sqrt((sqrt(3)*sqrt(256*a^3*d^3+27*a^2*b^4)+9*a*b^2)^(1/3)/(2^(1/3)*3^(2/3)*a)-(4*(2/3)^(1/3)*d)/(sqrt(3)*sqrt(256*a^3*d^3+27*a^2*b^4)+9*a*b^2)^(1/3)))-(sqrt(3)*sqrt(256*a^3*d^3+27*a^2*b^4)+9*a*b^2)^(1/3)/(2^(1/3)*3^(2/3)*a)+(4*(2/3)^(1/3)*d)/(sqrt(3)*sqrt(256*a^3*d^3+27*a^2*b^4)+9*a*b^2)^(1/3))-1/2*sqrt((sqrt(3)*sqrt(256*a^3*d^3+27*a^2*b^4)+9*a*b^2)^(1/3)/(2^(1/3)*3^(2/3)*a)-(4*(2/3)^(1/3)*d)/(sqrt(3)*sqrt(256*a^3*d^3+27*a^2*b^4)+9*a*b^2)^(1/3)) 
+
+#convert NaN to NA
+tb[which(is.na(tb))]=NA
+
+return(tb-273.15)
+}
---
+++ R/biophysmodel_Sceloporus.R
@@ -0,0 +1,99 @@
+#' @details Predicts body temperatures (operative environmental temperature) of a lizard in degrees C.
+#' @description Predicts body temperature (operative environmental temperature) of a lizard in degrees C. Based on Campbell and Norman (1998, An introduction to environmental biophysics). Designed for Sceloporus lizards and described in Buckley (2008, Linking traits to energetics and population dynamics to predict lizard ranges in changing environments. American Naturalist 171: E1-E19).
+#' 
+#' @param Ta is air temperature in C
+#' @param Tg  is surface temperature in C
+#' @param u is wind speed in m/s
+#' @param svl is lizard snout vent length in mm
+#' @param mass is lizard mass in g, note can be estimated as massfromsvl=function(svl) 3.55*10^-5*(svl)^3.00 #Tinkle and Ballinger 1972 
+#' @param psi is solar zenith angle in degrees
+#' @param rho_S is surface albedo (proportion)
+#' @param elevation is elevation in m
+#' @param doy is doy of year (1-366)
+#' @param sun indicates whether lizard is in sun (TRUE) or shade (FALSE)
+#' @param surface indicates whether lizard is on ground surface (TRUE) or above surface (FALSE, e.g. in a tree)
+#' @param alpha_S is lizard solar absorptivity, alpha_S=0.9 (Gates 1980, Table 11.4)
+#' @param alpha_L is lizard thermal absoptivity, alpha_L=0.965 (Bartlett & Gates 1967) 
+#' @param epsilon_s is surface emisivity of lizards, epsilon_s=0.965 (Bartlett & Gates 1967)
+#' @param F_d is the view factor between the surface of the lizard and diffuse solar radiation (Bartlett & Gates 1967)
+#' @param F_r is the view factor between the surface of the lizard and reflected solar radiation
+#' @param F_a is the view factor between the surface of the lizard and atmospheric radiation
+#' @param F_g is the view factor between the surface of the lizard and ground thermal radation
+#' 
+#' @keywords body temperature, biophysical model
+#' @export 
+#' \dontrun{
+#' estimate_lizard_Tb(Ta=25, Tg=30, u=0.1, svl=60, mass=10, psi=34, rho_S=0.7, elevation=500, doy=200, sun=TRUE, surface=TRUE, alpha_S=0.9, alpha_L=0.965, epsilon_s=0.965, F_d=0.8, F_r=0.5, F_a=0.5, F_g=0.5)
+#'}
+#' 
+estimate_lizard_Tb=function(Ta, Tg, u, svl, mass, psi, rho_S, elevation, doy, sun=TRUE, surface=TRUE, alpha_S=0.9, alpha_L=0.965, epsilon_s=0.965, F_d=0.8, F_r=0.5, F_a=0.5, F_g=0.5){
+  
+  psi= psi*pi/180 #convert zenith angle to radians
+  
+  # constants
+  sigma=5.67*10^-8 # stefan-boltzman constant, W m^-2 K^-4
+  c_p=29.3 # specific heat of air, J/mol degrees K or C
+  
+  tau=0.65 # atmospheric transmisivity
+  S_p0=1360 # extraterrestrial flux density, W/m^2 (p159)
+  
+  # Calculate radiation
+  # view angles, parameterize for animal suspended above ground (p181), on ground- adjust F_e, F_r, and F_g
+  h=svl/1000 # length of cylinder in m
+  theta = psi # angle between solar beam and a normal to the plane in radians, = psi for horizontal surfaces
+  
+  # F_p=(cos (theta)+(4*h*sin (theta))/(pi*d))/(2+4*h/d)  # beam view angle, Fig 11.6
+  A=0.121*mass^0.688   # total lizard area, roughgarden 1981 from Norris (1965) and Porter and James (1979)
+  A_p= (-1.1756810^-4*psi^2-9.2594*10^-2*psi+26.2409)*A/100      # projected area
+  F_p=A_p/A
+  
+  # radiation
+  p_a=101.3* exp (-elevation/8200)  # atmospheric pressure
+  m_a=p_a/(101.3*cos (psi))  # (11.12) optical air mass
+  m_a[which(psi>(80*pi/180))]=5.66
+  
+  # Flux densities
+  epsilon_ac= 9.2*10^-6*(Ta+273)^2 # (10.11) clear sky emissivity
+  L_a=epsilon_ac*sigma*(Ta+273)^4  # (10.7) long wave flux densities from atmosphere 
+  L_g=epsilon_s*sigma*(Tg+273)^4  # (10.7) long wave flux densities from ground
+  
+  #S_p=S_p0*tau^m_a # (11.11) direct irradience , W/m^2
+  dd2= 1+2*0.1675*cos(2*pi*doy/365)
+  S_p=S_p0*tau^m_a*dd2 *cos(psi)  #Sears and Angilletta 2012 #dd is correction factor accounting for orbit
+  
+  S_d=0.3*(1-tau^m_a)* S_p  # (11.13) diffuse radiation
+  #S_t=S_p*cos (psi)+S_d # solar irradience 
+  S_r= rho_S*S_p # (11.10) reflected radiation
+  
+  #__________________________________________________
+  # conductance
+  
+  dim=svl/1000 # characteristic dimension in meters (Table 9.5)
+  g_r= 4*sigma*(Ta+273)^3/c_p # (12.7) radiative conductance
+  
+  g_Ha=1.4*0.135*sqrt(u/dim) # boundary conductance, factor of 1.4 to account for increased convection (Mitchell 1976)
+  
+  #__________________________________________________
+  # operative environmental temperature
+  
+  #calculate with both surface and air temp (on ground and in tree)
+  
+  sprop=1 #proportion of radiation that is direct, Sears and Angilletta 2012
+  R_abs= sprop*alpha_S*(F_p*S_p+ F_d*S_d + F_r*S_r)+alpha_L*(F_a*L_a+F_g*L_g) # (11.14) Absorbed radiation
+  Te=Ta+(R_abs-epsilon_s*sigma*(Ta+273)^4)/(c_p*(g_r+g_Ha))                       
+  Te_surf= Tg+(R_abs-epsilon_s*sigma*(Tg+273)^4)/(c_p*(g_r+g_Ha))        
+  
+  # calculate in shade, no direct radiation
+  sprop=0 #proportion of radiation that is direct, Sears and Angilletta 2012
+  R_abs= sprop*alpha_S*(F_p*S_p+ F_d*S_d + F_r*S_r)+alpha_L*(F_a*L_a+F_g*L_g) # (11.14) Absorbed radiation
+  TeS=Ta+(R_abs-epsilon_s*sigma*(Ta+273)^4)/(c_p*(g_r+g_Ha))                       
+  TeS_surf=Tg+(R_abs-epsilon_s*sigma*(Tg+273)^4)/(c_p*(g_r+g_Ha))  
+  
+  #Select Te to return
+  if(sun==TRUE & surface==TRUE) Te= Te_surf
+  if(sun==TRUE & surface==FALSE) Te= Te
+  if(sun==FALSE & surface==TRUE) Te= TeS_surf
+  if(sun==FALSE & surface==FALSE) Te= TeS
+  
+  return(Te) 
+}
