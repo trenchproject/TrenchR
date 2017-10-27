4c0dc6107b8273b30ac5058f79a986b0193f542c
lbuckley <l.b.buckley@gmail.com>
2017-10-27 10:46:15 -0700

    Generalize functions.

--- R/energybalance_functions.R
+++ R/energybalance_functions.R
@@ -10,7 +10,8 @@
 #' @param mass Mass in grams.
 #' @param lambda Mean thickness.
 #' @param position Whether the organism is lying flat or standing.
-#' @param species Which species.
+#' @param K thermal conductivity #CHECK
+#' @param sa surface area in m^2
 #' @return conductance
 #' @keywords conductance
 #' @export
@@ -20,27 +21,21 @@
 #' }
 #' 
 
-conductance<-function(Ts,Ta, mass,lambda,position="flat", species="lizard"){
+conductance<-function(Ts,Ta, sa,lambda,K=0.5, position="flat"){
  
   # Assume Initial body temperature to air temperature
   To = Ta
   
-  # Set the conductivity
-  K = case_when(
-    species == "lizard" ~ 0.5,
-    TRUE ~ 0.1
-  )
-  
-  # Mass in kg
-  mass_kg=mass/1000. #in kg
-  
-  # Calculate surface area
-  A_L= 0.0314*3.14159*mass_kg^(2./3.) #surface area m2
+  ## Set the conductivity
+  #K = case_when(
+  #  taxa == "lizard" ~ 0.5,
+  #  TRUE ~ 0.1
+  #)
   
   # Calculate the area of contact
   A_contact = case_when(
-    position == "flat" ~ 0.35*A_L,
-    position == "standing" ~ 0.05*A_L
+    position == "flat" ~ 0.35*sa,
+    position == "standing" ~ 0.05*sa
   )
   
   #conduction
@@ -49,6 +44,31 @@ conductance<-function(Ts,Ta, mass,lambda,position="flat", species="lizard"){
   
   return(eb_conductance)
 }
+
+#' Calculate surface area from mass ##GENERALIZE TO MORE SPECIES
+#' 
+#' @details This function allows you to calculate surface area (m^2) from mass (g) for a variety of taxa
+#' @param mass Mass in grams.
+#' @param taxa Which class of organism, current choice: lizard.
+#' @return sa
+#' @keywords surface area
+#' @export
+#' @examples
+#'
+
+calculate_surface_area<-function(mass, taxa="lizard"){
+
+  #MAKE FUNCTION TO CALCULATE SURFACE AREA FROM BODY LENGTH
+  
+  # Mass in kg
+  mass_kg=mass/1000. #in kg
+  
+  # Calculate surface area
+  sa= 0.0314*3.14159*mass_kg^(2./3.) #surface area m2
+  
+  return(sa)
+}
+
 
 #' Calculate convection
 #' 
@@ -59,8 +79,8 @@ conductance<-function(Ts,Ta, mass,lambda,position="flat", species="lizard"){
 #' @details This function allows you to calculate convection of an ectotherm(Reptiles)
 #' @param Ts Surface Temperature.
 #' @param Ta Air Temperature.
-#' @param mass Mass in grams.
-#' @param species Which species.
+#' @param sa surface area in m^2
+#' @param K thermal conductivity #CHECK
 #' @return convection
 #' @keywords convection
 #' @export
@@ -70,30 +90,18 @@ conductance<-function(Ts,Ta, mass,lambda,position="flat", species="lizard"){
 #' }
 #' 
 
-convection<-function(Ts,Ta, mass, species="lizard"){
+convection<-function(Ts,Ta, sa,K=0.5){
   
   # Assume Initial body temperature to 0
   To = 0
   
   # Convective heat transfetr ceofficient (W m-2 K-1) 
   # (Porter et al. 1973)
-  # TODO Case statement for the heat transfer coefficient 
+  # TODO Case statement for the heat transfer coefficient, CHECK
   h_L=10.45 
   
-  # Set the conductivity
-  K = case_when(
-    species == "lizard" ~ 0.5,
-    TRUE ~ 0.1
-  )
-  
-  # Mass in kg
-  mass_kg=mass/1000. #in kg
-  
-  # Calculate surface area
-  A_L= 0.0314*3.14159*mass_kg^(2./3.) #surface area m2
-  
   #Calculate skin area exposed to air
-  Aair = 0.9*A_L # skin area that is exposed to air
+  Aair = 0.9*sa # skin area that is exposed to air
   
   #convection, assuming no wind
   eb_convection =   h_L*Aair*(Ta-To)
@@ -108,10 +116,10 @@ convection<-function(Ts,Ta, mass, species="lizard"){
 #' 
 #' 
 #' 
-#' @details This function allows you to calculate metabolic expense of an ectotherm(Reptiles)
-#' @param Ta Air Temperature.
+#' @details This function allows you to calculate basal or field metabolic rate (j/s) of various taxa (BMR or FMR depends on taxa)
+#' @param Tb body temperature in degrees C, currently only used for lizard.
 #' @param mass Mass in grams.
-#' @param species Which species.
+#' @param taxa Which taxa. options: lizard, reptile, bird, mammal
 #' @return metabolic expense
 #' @keywords metabolism
 #' @export
@@ -121,20 +129,40 @@ convection<-function(Ts,Ta, mass, species="lizard"){
 #' }
 #' 
 
-metabolic<-function(Ta, mass, species="lizard"){
+metabolic_rate<-function(Tb=NA, mass, taxa="lizard"){
   
-  # Assume Initial body temperature to air temperature
-  # Already in equilibrium 
-  To = Ta
-  
-  # Metabolism - Buckley 2008
+
+  # Metabolism - Buckley 2008, based on data for Sceloporus undulatus from Mike Angilletta
+  #Check x3 is for activity
   ew = case_when(
-    species == "lizard" ~ exp(-10.0+0.51*log(mass)+0.115*(To-273)) *3,
+    species == "lizard" ~ exp(-10.0+0.51*log(mass)+0.115*Tb ) *3/3600,
     TRUE ~ 1
   )
   
-  #metabolic rate (j/s)
-  eb_meta = ew/3600.
+  #Nagy 2005, JEB
+  #FMR in j/s, M is mass in grams
+  #Convert 1 kJ/day=0.0115741, check
+  
+  #reptiles
+  ew = case_when(
+    taxa == "reptile" ~ 0.196*mass^0.889 * 0.0115741,
+    TRUE ~ 1
+  )
+  
+  #mammals
+  ew = case_when(
+    taxa == "mammal" ~ 4.82*mass^0.734 * 0.0115741,
+    TRUE ~ 1
+  )
+  
+  #birds
+  ew = case_when(
+    taxa == "bird" ~ 10.5 * mass^0.681 * 0.0115741,
+    TRUE ~ 1
+  )
+  
+  #Can further divide using Nagy et al. 1999 ENERGETICS OF FREE-RANGING MAMMALS, REPTILES, AND BIRDS
+  #See whether can find coefficients to include temperature from Gillooly, Savage, etc.
   
   return(eb_meta)
 }
@@ -147,9 +175,9 @@ metabolic<-function(Ta, mass, species="lizard"){
 #' 
 #' @details This function allows you to calculate absorbed solar radiation of an ectotherm(Reptiles)
 
-#' @param mass Mass in grams.
+#' @param sa surface area in m^2
 #' @param solar Downward solar radiation(W/m^2).
-#' @param species Which species.
+#' @param thermal_abs thermal absorptivity as a percent, default value is for lizards, Bartlett & Gates 1967.
 #' @return solar radiation absorbed
 #' @keywords Solar radiation absorbed
 #' @export
@@ -159,29 +187,17 @@ metabolic<-function(Ta, mass, species="lizard"){
 #' }
 #' 
 
-solar_radiation_absorbed<-function( mass, solar, species="lizard"){
+solar_radiation_absorbed<-function( sa, solar,thermal_abs=0.965){
   
-  # Mass in kg
-  mass_kg=mass/1000. #in kg
-  
-  # Calculate surface area
-  A_L= 0.0314*3.14159*mass_kg^(2./3.) #surface area m2
-  
-
   # Absorbance of Lizard skin - thermal absoptivity, Bartlett & Gates 1967
   # Ranges between .95 and 1.0, Gates 1980
   # 
-  alpha_L = case_when(
-    species == "lizard" ~ 0.965,
-    TRUE ~ 1
-  )
-  
-  
+ 
   #projected lizard area for direct and scattered solar radiation
   A_p = 0.4*A_L 
   
   #solar radiation
-  eb_solar_radiation = alpha_L*A_p*solar
+  eb_solar_radiation = thermal_abs*A_p*solar
   
   
   return(eb_solar_radiation)
