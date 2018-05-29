4978fedd131befa259eefb360c06fa8e20f709df
Aji John <aji.john@gmail.com>
2018-05-29 12:19:51 -0700

    Availiable for all the functions

---
+++ R/Constants.R
@@ -0,0 +1,5 @@
+#' Constants
+#' 
+sigma=5.67*10^-8 # Stefan-Boltzman constant, W m^-2 K^-4
+c_p=29.3 # Specific heat of air, J/mol degrees K or C
+rd=180/pi  # factor to convert radians into degrees
