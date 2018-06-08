f892af9ceac4db3a61cb619c1aaf60771828ce8d
Aji John <aji.john@gmail.com>
2018-06-07 22:20:37 -0700

    Updated documentation

--- tests/testthat/test-dd-double-sine.R
+++ tests/testthat/test-dd-double-sine.R
@@ -1,7 +1,7 @@
 context("Degree Days::Double Sine")
 test_that("degree day function-double-sine work", {
   doublesine = read.csv("./degree-days-double-sine.csv")
-  doublesine$DD.Trench<-degree.days(doublesine$Air.min,doublesine$Air.max,12,33,"double.sine")
+  doublesine$DD.Trench<-degree_days(doublesine$Air.min,doublesine$Air.max,12,33,"double.sine")
   corrdoubsine=cor(doublesine$Degree.days,doublesine$DD.Trench,method = "pearson")
   expect_gte(corrdoubsine, .94)
   rm(doublesine)
