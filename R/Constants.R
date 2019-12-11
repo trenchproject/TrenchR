d077ed857bdae3f249369e46f6fb229b657d5a0f
L Buckley <lbbuckley@gmail.com>
2019-12-11 12:00:05 -0800

    Delete unneeded files

--- R/Constants.R
+++
@@ -1,7 +0,0 @@
-#' Constants
-#' 
-sigma=5.67*10^-8 # Stefan-Boltzman constant, W m^-2 K^-4
-c_p=29.3 # Specific heat of air, J/mol degrees K or C
-rd=180/pi  # factor to convert radians into degrees
-
-S_c=2 #Sc is the solar constant (2.0 cal cm ^-2 min ^-1)
