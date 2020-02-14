65afb2872c4ee315c46ecd40836d00c8359be4cd
Yutaro Sakairi <yutaro.sakairi@gmail.com>
2020-02-14 12:40:54 -0800

    improved function

--- R/biophysmodel_Sceloporus.R
+++ R/biophysmodel_Sceloporus.R
@@ -1,4 +1,4 @@
-#' Predicts body temperatures (operative environmental temperature) of a lizard in degrees C.
+#' Predicts body temperatures (operative environmental temperature) of a lizard in °C.
 #' 
 #' @details Predicts body temperatures (operative environmental temperature) of a lizard in °C.
 #' @description Predicts body temperature (operative environmental temperature) of a lizard in °C. 
@@ -11,7 +11,7 @@
 #' @param svl is lizard snout vent length in mm
 #' @param m is lizard mass in g, note that it can be estimated as massfromsvl=function(svl) 3.55*10^-5*(svl)^3.00 (Tinkle and Ballinger 1972)
 #' @param psi is solar zenith angle in degrees
-#' @param rho_S is surface albedo (proportion)
+#' @param rho_S is surface albedo (proportion). ~0.25 for grass, ~0.1 for dark soil, >0.75 for fresh snow (Campbell & Norman 1998)
 #' @param elev is elevation in m
 #' @param doy is day of year (1-366)
 #' @param sun indicates whether lizard is in sun (TRUE) or shade (FALSE)
@@ -29,7 +29,7 @@
 #' @export 
 #' @examples
 #' \dontrun{
-#' Tb_lizard(T_a=25, T_g=30, u=0.1, svl=60, m=10, psi=34, rho_S=0.7, elev=500, doy=200, sun=TRUE, surface=TRUE, alpha_S=0.9, alpha_L=0.965, epsilon_s=0.965, F_d=0.8, F_r=0.5, F_a=0.5, F_g=0.5)
+#' Tb_lizard(T_a=25, T_g=30, u=0.1, svl=60, m=10, psi=34, rho_S=0.24, elev=500, doy=200, sun=TRUE, surface=TRUE, alpha_S=0.9, alpha_L=0.965, epsilon_s=0.965, F_d=0.8, F_r=0.5, F_a=0.5, F_g=0.5)
 #'}
 #' 
 Tb_lizard=function(T_a, T_g, u, svl, m, psi, rho_S, elev, doy, sun=TRUE, surface=TRUE, alpha_S=0.9, alpha_L=0.965, epsilon_s=0.965, F_d=0.8, F_r=0.5, F_a=0.5, F_g=0.5){
@@ -40,18 +40,16 @@ Tb_lizard=function(T_a, T_g, u, svl, m, psi, rho_S, elev, doy, sun=TRUE, surface
   
   # constants
   sigma=5.67*10^-8 # stefan-boltzman constant, W m^-2 K^-4
-  c_p=29.3 # specific heat of air, J/mol degrees K or C
+  c_p=29.3 # specific heat of air, J/mol C (p.279) Parentheses all from Campbell & Norman 1998
   
   tau=0.65 # atmospheric transmisivity
-  S_p0=1360 # extraterrestrial flux density, W/m^2 (p159)
+  S_p0=1360 # extraterrestrial flux density, W/m^2 (p.159)
   
   # Calculate radiation
   # view angles, parameterize for animal suspended above ground (p181), on ground- adjust F_e, F_r, and F_g
-  h=svl/1000 # length of cylinder in m
-  theta = psi # angle between solar beam and a normal to the plane in radians, = psi for horizontal surfaces
+  h=svl/1000 # length of svl in m
   
-  # F_p=(cos (theta)+(4*h*sin (theta))/(pi*d))/(2+4*h/d)  # beam view angle, Fig 11.6
-  A=0.121*m^0.688   # total lizard area, roughgarden 1981 from Norris (1965) and Porter and James (1979)
+  A=0.121*m^0.688   # total lizard area, Roughgarden (1981)
   A_p= (-1.1756810^-4*psi^2-9.2594*10^-2*psi+26.2409)*A/100      # projected area
   F_p=A_p/A
   
@@ -62,22 +60,20 @@ Tb_lizard=function(T_a, T_g, u, svl, m, psi, rho_S, elev, doy, sun=TRUE, surface
   
   # Flux densities
   epsilon_ac= 9.2*10^-6*(T_a+273)^2 # (10.11) clear sky emissivity
-  L_a=epsilon_ac*sigma*(T_a+273)^4  # (10.7) long wave flux densities from atmosphere 
-  L_g=epsilon_s*sigma*(T_g+273)^4  # (10.7) long wave flux densities from ground
+  L_a=sigma*(T_a+273)^4  # (10.7) long wave flux densities from atmosphere 
+  L_g=sigma*(T_g+273)^4  # (10.7) long wave flux densities from ground
   
-  #S_p=S_p0*tau^m_a # (11.11) direct irradience , W/m^2
-  dd2= 1+2*0.1675*cos(2*pi*doy/365)
-  S_p=S_p0*tau^m_a*dd2 *cos(psi)  #Sears and Angilletta 2012 #dd is correction factor accounting for orbit
-  
-  S_d=0.3*(1-tau^m_a)* S_p  # (11.13) diffuse radiation
-  #S_t=S_p*cos (psi)+S_d # solar irradience 
-  S_r= rho_S*S_p # (11.10) reflected radiation
+  S_d=0.3*(1-tau^m_a)* S_p0 * cos(psi)  # (11.13) diffuse radiation
+  S_p = S_p0 * tau^m_a
+  S_b = S_p * cos(psi)
+  S_t = S_b + S_d
+  S_r= rho_S*S_t # (11.10) reflected radiation
   
   #__________________________________________________
   # conductance
   
-  dim=svl/1000 # characteristic dimension in meters (Table 9.5)
-  g_r= 4*sigma*(T_a+273)^3/c_p # (12.7) radiative conductance
+  dim=svl/1000 # characteristic dimension in meters
+  g_r= 4*epsilon_s*sigma*(T_a+273)^3/c_p # (12.7) radiative conductance
   
   g_Ha=1.4*0.135*sqrt(u/dim) # boundary conductance, factor of 1.4 to account for increased convection (Mitchell 1976)
   
@@ -88,13 +84,13 @@ Tb_lizard=function(T_a, T_g, u, svl, m, psi, rho_S, elev, doy, sun=TRUE, surface
   
   sprop=1 #proportion of radiation that is direct, Sears and Angilletta 2012
   R_abs= sprop*alpha_S*(F_p*S_p+ F_d*S_d + F_r*S_r)+alpha_L*(F_a*L_a+F_g*L_g) # (11.14) Absorbed radiation
-  Te=T_a+(R_abs-epsilon_s*sigma*(T_a+273)^4)/(c_p*(g_r+g_Ha))                       
+  Te=T_a+(R_abs-epsilon_s*sigma*(T_a+273)^4)/(c_p*(g_r+g_Ha))         # (12.19) Operative temperature            
   Te_surf= T_g+(R_abs-epsilon_s*sigma*(T_g+273)^4)/(c_p*(g_r+g_Ha))        
   
   # calculate in shade, no direct radiation
   sprop=0 #proportion of radiation that is direct, Sears and Angilletta 2012
   R_abs= sprop*alpha_S*(F_p*S_p+ F_d*S_d + F_r*S_r)+alpha_L*(F_a*L_a+F_g*L_g) # (11.14) Absorbed radiation
-  TeS=T_a+(R_abs-epsilon_s*sigma*(T_a+273)^4)/(c_p*(g_r+g_Ha))                       
+  TeS=T_a+(R_abs-epsilon_s*sigma*(T_a+273)^4)/(c_p*(g_r+g_Ha))         # (12.19) Operative temperature                        
   TeS_surf=T_g+(R_abs-epsilon_s*sigma*(T_g+273)^4)/(c_p*(g_r+g_Ha))  
   
   #Select Te to return
