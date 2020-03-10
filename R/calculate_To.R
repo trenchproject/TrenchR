6c2f06789828436af9ed0beabd87f76c9802e264
Yutaro Sakairi <yutaro.sakairi@gmail.com>
2020-03-10 13:14:12 -0700

    edited Tb_Fei

--- R/calculate_To.R
+++ R/calculate_To.R
@@ -10,15 +10,16 @@
 #' @param lw Downward flux of near-infrared radiation (W/m^2)
 #' @param shade proportion of shade
 #' @param m is lizard mass in g
-#' @param Acondfact is the proportion of the lizard projected area that is in contact with the ground, Acondfact=0.05 for standing and Acondfact=0.35 for lying on ground
-#' @param Agradfact is the proportion of the lizard projected area exposed to radiation from the ground, Agradfact=0.4 for standing and Agradfact=0.0 for lying on ground
+#' @param Acondfact is the proportion of the lizard projected area that is in contact with the ground, Acondfact=0.1 for standing and Acondfact=0.4 for lying on ground
+#' @param Agradfact is the proportion of the lizard projected area exposed to radiation from the ground, Agradfact=0.3 for standing and Agradfact=0.0 for lying on ground
+#' @return Body temperature of a lizard in K
 #' @keywords Operative Temperature Fei
 #' @family biophysical models
 #' @author Ofir Levy
 #' @export 
 #' @examples
 #' \dontrun{
-#' Tb_Fei(T_a=293, T_g=300, H=600, lw=30, shade=0.5, m=10.5, Acondfact=0.05, Agradfact=0.4)
+#' Tb_Fei(T_a=293, T_g=300, H=1300, lw=60, shade=0.5, m=10.5, Acondfact=0.1, Agradfact=0.3)
 #' }
 #' 
 
@@ -28,11 +29,11 @@ Tb_Fei <- function(T_a, T_g, H, lw, shade, m, Acondfact, Agradfact){
   
   # ---- Start Constants
   alpha_L = 0.965 # thermal absoptivity (proportion), Bartlett & Gates 1967
-  h_L=10.45 #convective heat transfer ceofficient (W m-2 K-1) (Fei et al. 2012, J Ther Biol, 37: 56-64, Porter et al. 1973?)
+  h_L = 10.45 #convective heat transfer ceofficient (W m-2 K-1) (Fei et al. 2012, J Ther Biol, 37: 56-64, Porter et al. 1973?)
   epsilon_lizard=0.95 # emissivity of lizard's skin (proportion)
-  K_lizard = 0.5 #thermal conductivity (W K-1 m-1)
-  lambda = 0.02 # lizard mean thickness in meters (diameter)
-  c_lizard = 3762 #specific heat capacity (J kg-1)
+  K_lizard = 0.5 # thermal conductivity (W K-1 m-1)
+  lambda = 0.015 # lizard mean thickness in meters (diameter)
+  c_lizard = 3762 #specific heat capacity (J kg-1 K-1)
   dt =  120 #time interval in seconds
   
   sigma=5.67*10.0^(-8) # stefan-boltzmann constant (W m^-2 K^-4)
@@ -41,7 +42,7 @@ Tb_Fei <- function(T_a, T_g, H, lw, shade, m, Acondfact, Agradfact){
   mass_kg=m/1000. #in kg
   
   #Estimate areas
-  A_L= 0.0314*3.14159*mass_kg^(2./3.) #surface area m2
+  A_L= 0.0314 * pi * mass_kg^(2./3.) #surface area m2
   #Estimate projected lizard area for radiation from the ground
   A_down=Agradfact*A_L 
   #Estimate projected lizard area that contacts the ground
@@ -53,32 +54,50 @@ Tb_Fei <- function(T_a, T_g, H, lw, shade, m, Acondfact, Agradfact){
   
   #solar radiation
   A_p = 0.4*A_L #projected lizard area for direct and scattered solar radiation
+  
   #check
   dQ_solar = (1-shade)*alpha_L*A_p*H
   
   #net longwave radiation
   A_up = 0.6*A_L #proportion of surface area facing toward the sky
   epsilon_ac= 9.2*10^-6*(T_a)^2 # (10.11) clear sky emissivity
-  for (i in 10) {
-    dQ_IR = epsilon_lizard*A_down*sigma*(T_g^4. - T_o^4.) + epsilon_lizard*A_up*((1-shade)*lw + shade*sigma*T_a^4.) - epsilon_lizard*A_up*sigma*T_o^4.
-    
-    #conduction
-    dQ_cond = A_contact*K_lizard*(T_g - T_o)/(lambda/2)
-    
-    #convection, assuming no wind
-    Aair = 0.9*A_L # skin area that is exposed to air
-    dQ_conv=h_L*Aair*(T_a-T_o)
-    
-    #Metabolism
-    ew = exp(-10.0+0.51*log(m)+0.115*(T_o-273)) *3 #Buckley 2008
-    dQ_meta = ew/3600. #metabolic rate (j/s)
-    
-    dQe = (dQ_solar + dQ_IR + dQ_meta + dQ_cond + dQ_conv)
-    
-    dTe = dQe/((mass_kg)*c_lizard)
-    T_o = T_o + dTe*dt 
-    
+  
+  # different from the paper. source?
+  dQ_IR = epsilon_lizard*A_down*sigma*(T_g^4. - T_o^4.) + epsilon_lizard*A_up*((1-shade)*lw + shade*sigma*T_a^4.) - epsilon_lizard*A_up*sigma*T_o^4.
+  # dQ_IR = epsilon_lizard*A_down*sigma*(T_g^4. - T_o^4.) + epsilon_lizard*A_up*sigma*(T_a^4. - T_o^4.) is the original function
+  # but it's also not accurate because we set T_o = T_a and the second half of the equation is always 0.
+  
+  #conduction
+  dQ_cond = A_contact*K_lizard*(T_g - T_o)/(lambda/2)
+
+  #convection, assuming no wind
+  # this is always 0 because we set T_o = T_a
+  Aair = 0.9 * A_L # skin area that is exposed to air
+  dQ_conv = h_L * Aair * (T_a - T_o)
+
+  #Metabolism
+  # ew = exp(-10.0+0.51*log(m)+0.115*(T_o-273)) *3 #Buckley 2008
+  # dQ_meta = ew/3600. #metabolic rate (j/s)
+  
+  TinC = T_o - 273.15
+  dQ_meta = 0.348 * exp(0.022 * TinC - 0.132) * mass_kg
+  
+  
+  #Respiratory loss
+  if (TinC <20) {
+    dQ_resp = 0.272 * mass_kg
+  } else if (TinC > 36) {
+    dQ_resp = 0.003 * mass_kg * exp(0.1516 * TinC)
+  } else {
+    dQ_resp = 0.0836 * mass_kg * exp(0.0586 * TinC)
   }
   
+  dQe = (dQ_solar + dQ_IR + dQ_meta + dQ_cond + dQ_conv - dQ_resp)
+
+  dTe = dQe/((mass_kg)*c_lizard)
+  T_o = T_o + dTe*dt
+    
   return (T_o)
 }
+
+solve(5 * x, 1)
