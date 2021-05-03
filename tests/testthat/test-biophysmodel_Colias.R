764ddbd6119c6eafd4ccfd075686e1ed49732970
Lauren Buckley <lbbuckley@gmail.com>
2021-05-03 15:40:00 -0700

    Minor fixes. Including reverting an erroneous parameter value change in the butterfly biophysical model.

--- tests/testthat/test-biophysmodel_Colias.R
+++ tests/testthat/test-biophysmodel_Colias.R
@@ -5,5 +5,5 @@ expect_similar <- function(input, expected) {
 }
 
 test_that("Tb_butterfly function works as expected", {
-  expect_similar(Tb_butterfly(T_a=25, Tg=25, Tg_sh=20, u=0.4, H_sdir=300, H_sdif=100, z=30, D=0.36, delta=1.46, alpha=0.6, r_g=0.3), 22.07)
+  expect_similar(Tb_butterfly(T_a=25, Tg=25, Tg_sh=20, u=0.4, H_sdir=300, H_sdif=100, z=30, D=0.36, delta=1.46, alpha=0.6, r_g=0.3), 29.17)
 })
