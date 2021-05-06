a2442f76a9d0ac1521910085001c06b8ed60654b
Juniper Simonis <dapperstats@gmail.com>
2021-05-05 19:15:54 -0700

    create R and Rd files for package
    setting up a basic package `Rd` file via a `TrenchR.R` file
    editing the `DESCRIPTION` file to match
    
    TO DO: develop a CRAN-typical package description

---
+++ R/TrenchR.R
@@ -0,0 +1,11 @@
+#' @title Assessing the Impact of the Environment of Organisms
+#'
+#' @description Resources for assessing the impact of the environment of organisms
+#'
+#' @name TrenchR
+#'
+#' @docType package
+#'
+#' @keywords package
+#'
+NULL
