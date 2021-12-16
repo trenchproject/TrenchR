cb0f628b42b889a534f27923141b07c6849a3ae1
Juniper Simonis <dapperstats@gmail.com>
2021-12-16 16:31:05 -0600

    vignette edits to achieve full build
    TeTutorial
    removing function marked for it
    updating function namings
    commented out function with the gamma issue raised in #61

--- vignettes/AllometryAndConversionsTutorial.R
+++
@@ -1,7 +0,0 @@
-## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-
-## -----------------------------------------------------------------------------
-##Load TrenchR package
-library(TrenchR)
-
