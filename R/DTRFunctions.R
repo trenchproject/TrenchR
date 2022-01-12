574239d23dc08467700f6e3a4c41ff74b59d8954
Abby Meyer <ameyer@Abbys-MacBook-Pro.local>
2022-01-12 13:29:40 -0500

    DTRfunctions code cleanup

--- R/DTRFunctions.R
+++ R/DTRFunctions.R
@@ -1,24 +1,24 @@
 #' @title Estimate temperature across hours using a diurnal temperature variation function incorporating sine and exponential components 
 #'
-#' @description  estimate temperature across hours using a diurnal temperature variation function incorporating sine and exponential components. Source: Parton WJ and Logan JA. 1981. A model for diurnal variation in soil and air temperature. Agricultural Meteorology 23: 205-216.
+#' @description  estimate temperature across hours using a diurnal temperature variation function incorporating sine and exponential components. Source: \insertCite{Parton1981}{TrenchR}. Default alpha, beta, gamma values are the average of 5 North Carolina sites  \insertCite{Wann1985}{TrenchR}. Other alpha, beta, gamma parameterizations include values for Denver, Colorado from \insertCite{Parton1981}{TrenchR}: 150cm air temperature: alpha = 1.86, beta = 2.20, gamma = -0.17; 10cm air temperature: alpha = 1.52, beta = 2.00, gamma = -0.18; soil surface temperature: alpha = 0.50, beta = 1.81, gamma = 0.49; 10cm soil temperature: alpha = 0.45, beta = 2.28, gamma = 1.83.
 #' 
-#' @param T_max maximum daily temperature (C)
+#' @param T_max \code{numeric} maximum daily temperature (C)
 #' 
-#' @param T_min minimum daily temperature (C)
+#' @param T_min \code{numeric} minimum daily temperature (C)
 #' 
-#' @param t_s time of sunrise (hour)
+#' @param t_s \code{numeric} time of sunrise (hour)
 #' 
-#' @param t_r time of sunset (hour)
+#' @param t_r \code{numeric} time of sunset (hour)
 #' 
-#' @param t time for temperature estimate (hour)
+#' @param t \code{numeric} time for temperature estimate (hour)
 #' 
-#' @param alpha  time difference between t_x (time of maximum temperature) and noon (hour)
+#' @param alpha \code{numeric} time difference between t_x (time of maximum temperature) and noon (hour). Defaults to 2.59
 #' 
-#' @param gamma decay parameter for rate of t change from sunset to t_n (time of minimum temp)
+#' @param gamma \code{numeric} decay parameter for rate of t change from sunset to t_n (time of minimum temp). Defaults to 2.2
 #' 
-#' @param beta time difference between t_x and sunrise (hour)
+#' @param beta \code{numeric} time difference between t_x and sunrise (hour). Defaults to 1.55
 #' 
-#' @return temperature (C) at a specified hour 
+#' @return \code{numeric} temperature (C) at a specified hour 
 #' 
 #' @keywords Temperature
 #' 
@@ -26,65 +26,64 @@
 #' 
 #' @export
 #' 
-#' @examples
-#'   diurnal_temp_variation_sineexp(
-#'   T_max=30, 
-#'   T_min=10, 
-#'   t=11, 
-#'   t_r=6, 
-#'   t_s=18, 
-#'   alpha=2.59, 
-#'   beta= 1.55, 
-#'   gamma=2.2)
+#' @references
+#'   \insertAllCited{}
 #' 
-diurnal_temp_variation_sineexp=function(T_max, T_min, t, t_r, t_s, alpha=2.59, beta= 1.55, gamma=2.2){
+#' @examples
+#'   diurnal_temp_variation_sineexp(T_max = 30, T_min = 10, t = 11, t_r = 6, t_s = 18, alpha = 2.59, beta = 1.55, gamma = 2.2)
+#' 
+diurnal_temp_variation_sineexp <- function(T_max, T_min, t, t_r, t_s, alpha = 2.59, beta = 1.55, gamma = 2.2){
 
-  stopifnot(T_max>=T_min, t_s>=0, t_s<=24,t_r>=0, t_r<=24,t>=0, t<=24,alpha>=0, alpha<=24,gamma>=0, gamma<=24,beta>=0, beta<=24)
+  stopifnot(T_max >= T_min, t_s >= 0, t_s <= 24,t_r >= 0, t_r <= 24,t >= 0, t <= 24, alpha >= 0, alpha <= 24,gamma >= 0, gamma <= 24,beta >= 0, beta <= 24)
+   
+  l <- t_s - t_r #daylength
   
-  #default values are the average of 5 North Carolina sites. Source: Wann M et al. 1985. Evaluation and calibration of three models for daily cycle of air temperature. Agricultural and Forest Meteorology 34: 121-128.
+  T_x <- 0.5 * (t_r + t_s) + alpha #time of maximum temperature
+  T_n <- t_r +  beta #time of minimum temperature
   
-  #Other parameterizations include values for Denver, Colorado from Parton and Logan (1981): 
-  # 150cm air temeprature: alpha=1.86, beta= 2.20, gamma=-0.17
-  # 10cm air temperature: alpha=1.52, beta= 2.00, gamma=-0.18
-  # soil surface temperature: alpha=0.50, beta= 1.81, gamma=0.49
-  # 10cm soil temperature: alpha=0.45, beta= 2.28, gamma=1.83
- 
-l= t_s-t_r #daylength
-
-T_x= 0.5*(t_r+t_s)+alpha #time of maximum temperature
-T_n= t_r+ beta #time of minimum temperature
-
-#calculate temperature for nighttime hour
-if(!(t > (t_r + beta) & t < t_s)) {
-  T_sn = T_min + (T_max - T_min) * sin((pi * (t_s - t_r - beta)) / (l + 2 * (alpha -
-                                                                      beta)))
-  if (t <= (t_r + beta))
-    Tas = t + 24 - t_s
-  if (t >= t_s)
-    Tas = t - t_s  #time after sunset
-  T = T_min + (T_sn - T_min) * exp(-(gamma * Tas) / (24 - l + beta))
+  # calculate temperature for nighttime hour
+  if(!(t > (t_r + beta) & t < t_s)) {
+    T_sn <- T_min + (T_max - T_min) * sin((pi * (t_s - t_r - beta)) / (l + 2 * (alpha -beta)))
+    
+    if (t <= (t_r + beta)) {
+      
+      Tas <- t + 24 - t_s
+      
+    }
+      
+    if (t >= t_s) {
+      
+      Tas <- t - t_s  #time after sunset
+      
+    }
+    
+    T <- T_min + (T_sn - T_min) * exp(-(gamma * Tas) / (24 - l + beta))
+    
+  }
+  
+  # calculate temperature for daytime hour
+  if(t > (t_r + beta) & t < t_s) {
+    
+    T <- T_min + (T_max - T_min) * sin((pi * (t - t_r - beta)) / (l + 2 * (alpha - beta)))
+    
+  }
+  
+  T
+  
 }
 
-#calculate temperature for daytime hour
-if(t>(t_r + beta) &
-   t<t_s) {
-  T = T_min + (T_max - T_min) * sin((pi * (t - t_r - beta)) / (l + 2 * (alpha -
-                                                                    beta)))
-}
-return(T)
-}
 
 #' @title Diurnal temperature across hours
 #'
-#' @description  estimate temperature for a specified hour using the sine interpolation in Campbell and Norman (1998). Source: Campbell and Norman. 1998. An Introduction to Environmental Biophysics.
+#' @description estimate temperature for a specified hour using the sine interpolation in \insertCite{Campbell1998}{TrenchR}.
 #' 
-#' @param T_max maximum daily temperature (C) 
+#' @param T_max \code{numeric} maximum daily temperature (C) 
 #' 
-#' @param T_min minimum daily temperature (C)
+#' @param T_min \code{numeric} minimum daily temperature (C)
 #' 
-#' @param t time for temperature estimate (hour)
+#' @param t \code{numeric} time for temperature estimate (hour)
 #' 
-#' @return temperature (C) at a specified hour 
+#' @return \code{numeric} temperature (C) at a specified hour 
 #' 
 #' @keywords Temperature
 #' 
@@ -92,81 +91,103 @@ return(T)
 #' 
 #' @export
 #' 
+#' @references
+#'   \insertAllCited{}
+#' 
 #' @examples
+#' diurnal_temp_variation_sine(T_max = 30, T_min = 10, t = 11)
 #' 
-#' diurnal_temp_variation_sine(T_max=30, T_min=10, t=11)
-#' 
-diurnal_temp_variation_sine=function(T_max, T_min, t){
+diurnal_temp_variation_sine <- function(T_max, T_min, t){
   
-  stopifnot(t>=0, t<=24, T_max>=T_min)
+  stopifnot(t >= 0, t <= 24, T_max >= T_min)
   
-  W=pi/12;
-  gamma= 0.44 - 0.46* sin(0.9 + W * t)+ 0.11 * sin(0.9 + 2 * W * t);   # (2.2) diurnal temperature function
-  T = T_max*gamma + T_min * (1 - gamma)
+  W <- pi / 12
+  gamma <- 0.44 - 0.46 * sin(0.9 + W * t) + 0.11 * sin(0.9 + 2 * W * t)   # (2.2) diurnal temperature function
+    
+  T_max * gamma + T_min * (1 - gamma)
   
-  return(T)
 }
 
 
-#' Estimates temperature across hours using sine and square root functions
+#' @title Estimates temperature across hours using sine and square root functions
 #'
 #' @details  Estimates temperature across hours using sine and square root functions
 #' 
-#' @description  estimate temperature for a specified hour using sine and square root functions. Source: Cesaraccio C et al. 2001. An improved model for determining degree-day values from daily temperature data. International Journal of Biometeorology 45:161-169.
+#' @description  estimate temperature for a specified hour using sine and square root functions. Source: \insertCite{Cesaraccio2001}{TrenchR}
 #' 
-#' @param t hour or hours for temperature estimate
+#' @param t \code{numeric} hour or hours for temperature estimate
 #' 
-#' @param tr sunrise hour (0-23)
+#' @param tr \code{numeric} sunrise hour (0-23)
 #' 
-#' @param ts sunset hour (0-23)
+#' @param ts \code{numeric} sunset hour (0-23)
 #' 
-#' @param T_max maximum temperature of current day (C) 
+#' @param T_max \code{numeric} maximum temperature of current day (C) 
 #' 
-#' @param T_min minimum temperature of current day (C)
+#' @param T_min \code{numeric} minimum temperature of current day (C)
 #' 
-#' @param T_minp minimum temperature of following day (C)
+#' @param T_minp \code{numeric} minimum temperature of following day (C)
 #' 
-#' @return temperature (C) at a specified hour 
+#' @return \code{numeric} temperature (C) at a specified hour 
 #' 
 #' @keywords Temperature
 #' 
 #' @family microclimate functions
 #' 
 #' @export
+#' 
+#' @references
+#'   \insertAllCited{}
 #' 
 #' @examples
 #' diurnal_temp_variation_sinesqrt(t=8, tr=6, ts=18, T_max=30, T_min=10, T_minp=12)
 #' 
-diurnal_temp_variation_sinesqrt=function(t, tr, ts, T_max, T_min, T_minp){
+diurnal_temp_variation_sinesqrt <- function(t, tr, ts, T_max, T_min, T_minp){
  
-  stopifnot(t>=0, t<=24, tr>=0, tr<=24, ts>=0, ts<=24, T_max>=T_min)
+  stopifnot(t >= 0, t <= 24, tr >= 0, tr <= 24, ts >= 0, ts <= 24, T_max >= T_min)
   
-  #Time estimates
-  tp = tr + 24 #sunrise time following day
-  tx= ts - 4 #Assume time of maximum temperature 4h before sunset
+  # Time estimates
+  tp <- tr + 24 # sunrise time following day
+  tx <- ts - 4 # Assume time of maximum temperature 4h before sunset
   
-  #Temperature at sunset
-  c=0.39 #empircally fitted parameter
-  To= T_max - c*(T_max-T_minp)
+  # Temperature at sunset
+  c <- 0.39 # empircally fitted parameter
+  To <- T_max - c * (T_max - T_minp)
   
-  alpha= T_max -T_min
-  R = T_max - To
-  b= (T_minp - To)/sqrt(tp -ts)
+  alpha <- T_max -T_min
+  R <- T_max - To
+  b <- (T_minp - To) / sqrt(tp - ts)
   
-  T= rep(NA, length(t))
+  T <- rep(NA, length(t))
   
-  inds=which(t<= tr) 
-  ##t > ts & t <= tp ?
-  if(length(inds>0))  T[inds]= To+b*sqrt(t[inds]-(ts-24) )
-  ##t-ts? instead of t-(ts-24)?
-  inds=which(t>tr & t<=tx) 
-  if(length(inds>0)) T[inds]= T_min+ alpha*sin(pi/2*(t[inds]-tr)/(tx-tr))
-  ##delete sin
-  inds=which(t>tx & t<ts) 
-  if(length(inds>0)) T[inds]= To+ R*sin(pi/2*(1+ (t[inds]-tx)/4) )
+  inds <- which(t <= tr) 
+
+  if(length(inds>0))  {
+    
+    T[inds] <- To + b * sqrt(t[inds] - (ts - 24))
+    
+  }
+
+  inds <- which(t > tr & t <= tx) 
+  if(length(inds > 0)) {
+    
+    T[inds] <- T_min + alpha * sin(pi / 2 * (t[inds] - tr) / (tx - tr))
+    
+  }
   
-  inds=which(t>=ts) 
-  if(length(inds>0))  T[inds]= To+b*sqrt(t[inds]-ts)
-  ##delete
-  return(T)
+  inds <- which(t > tx & t < ts) 
+  if(length(inds > 0)) {
+    
+    T[inds] <- To + R * sin(pi / 2 * (1 + (t[inds] - tx) / 4))
+    
+  }
+  
+  inds <- which(t >= ts) 
+  if(length(inds > 0))  {
+    
+    T[inds] <- To + b * sqrt(t[inds] - ts)
+    
+  }
+
+  T
+  
 }
